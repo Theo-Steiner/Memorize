2c0a255651222642a84c0adff8e8cbbeb12c129f
Theodor Steiner <theosteiner@gmail.com>
2024-08-10 15:31:46 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Theo Steiner on 2024/08/10.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
