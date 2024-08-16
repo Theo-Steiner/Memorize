45bbf45bbd71d7a6db951ed93602ddb31abd511c
Theodor Steiner <theosteiner@gmail.com>
2024-08-17 00:04:53 +0900

    feat: implement Pie shape

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -37,7 +37,11 @@ struct CardView: View {
                 if card.isFaceUp {
                     shape.fill().foregroundColor(.white)
                     shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Circle()
+                    Pie(
+                        // subtract 90 from angles, since 0 degs is to the right in SwiftUI
+                        startAngle: Angle(degrees: 0 - 90),
+                        endAngle: Angle(degrees: 110 - 90)
+                    ).padding(5).opacity(0.5)
                     Text(card.content).font(calculateFontSize(in: geometry.size))
                 } else {
                     shape.fill()
@@ -53,12 +57,13 @@ struct CardView: View {
     private struct DrawingConstants {
         static let cornerRadius: CGFloat = 20
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.8
+        static let fontScale: CGFloat = 0.7
     }
 }
 
 #Preview {
     let game = EmojiMemoryGame()
-    
+    let _ = game.choose(game.cards.first!)
+
     EmojiMemoryGameView(game: game)
 }
